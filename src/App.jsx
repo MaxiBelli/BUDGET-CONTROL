eb3c2d96524d7d6993e97600d9e3740ce0be9720
MaxiBelli <maximiliano_belli@hotmail.com>
2023-04-24 20:43:38 -0300

    feat: Create NewBudget and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-
-
+import Header from "./components/Header";
 
 function App() {
- 
-
   return (
-   <h1>Budget Control</h1>
-  )
+    <div>
+      {" "}
+      <Header />{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import NewBudget from "./NewBudget";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Expense Planner</h1>
+
+      <NewBudget />
+
+    </header>
+  );
+};
+
+export default Header;
