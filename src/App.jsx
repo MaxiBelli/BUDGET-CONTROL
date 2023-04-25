ea5142781dd1fd81dc0ff27baae72f4e10b0c8bd
MaxiBelli <maximiliano_belli@hotmail.com>
2023-04-25 16:53:24 -0300

    feat: Add Modal Component with Open/Close Modal Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+import NewExpenseIcon from "./img/new-expense.svg";
 
 function App() {
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  const handleNewExpense = () => {
+    setModal(true);
+  };
 
   return (
     <div>
@@ -14,6 +22,16 @@ function App() {
         isValidBudget={isValidBudget}
         setIsValidBudget={setIsValidBudget}
       />{" "}
+      {isValidBudget && (
+        <div className="new-expense">
+          <img
+            src={NewExpenseIcon}
+            alt="new expense icon"
+            onClick={handleNewExpense}
+          />
+        </div>
+      )}
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,19 @@
+
+import CloseBtn from "../img/close.svg";
+
+const Modal = ({setModal}) => {
+
+  const hideModal = () => {
+    setModal(false);
+  };
+
+  return (
+    <div className="modal">
+      <div className="close-modal">
+        <img src={CloseBtn} alt="close modal" onClick={hideModal} />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
