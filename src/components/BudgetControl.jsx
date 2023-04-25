c7fb9b78023f18d7c7984fc45c0419d762ca2a0d
MaxiBelli <maximiliano_belli@hotmail.com>
2023-04-25 15:30:57 -0300

    feat: Add Budget Control component

---
+++ src/components/BudgetControl.jsx
@@ -0,0 +1,16 @@
+const BudgetControl = ({ budget }) => {
+  return (
+    <div className="budget-container container shadow two-columns">
+      <div>
+        <p>Graph here</p>
+      </div>
+      <div className="budget-content">
+        <p>
+          <span>Budget:</span> ${budget}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BudgetControl;
